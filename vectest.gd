8831953a19d4e060fa8ca43f58def109ad707bb2
Terkwood <metaterkhorn@gmail.com>
2019-12-06 11:45:26 -0500

    Update vectest.gd

--- vectest.gd
+++ vectest.gd
@@ -12,21 +12,40 @@ func _ready():
 	var d = 0
 	var pos = Vector2(0,0)
 	
+	# step 0
 	l.add_point(pos)
-	var p1 = pos + STEP
-	var p2 = p1 + STEP
-	var p3 = STEP.rotated(-DELTA + -DELTA) + p2
-	var p4 = STEP.rotated(-DELTA + -DELTA + DELTA) + p3
-	var p5 = STEP.rotated(-DELTA + -DELTA + DELTA + DELTA) + p4
 	
-	l.add_point(p1)
-	l.add_point(p2)
-	l.add_point(p3)
-	l.add_point(p4)
-	l.add_point(p5)
-
-	print(p1)
-	print(p2)
-	print(p3)
+	# step 1
+	pos = pos + STEP
+	l.add_point(pos)
+	
+	# step 2
+	pos = pos + STEP
+	l.add_point(pos)
+	
+	# step 3
+	d = d + -DELTA
+	
+	# step 4
+	d = d + -DELTA
+	
+	# step 5
+	pos = STEP.rotated(d) + pos
+	l.add_point(pos)
+	
+	# step 6
+	d = d + DELTA
+	
+	# step 7
+	pos = STEP.rotated(d) + pos
+	l.add_point(pos)
+	
+	# step 8
+	d = d + DELTA
+	
+	# step 9
+	pos = STEP.rotated(d) + pos
+	l.add_point(pos)
+	
 	
 	add_child(l)
