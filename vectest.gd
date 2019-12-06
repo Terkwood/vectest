5dfae93da8d5d9309bd265446b730167cfe222ac
Terkwood <metaterkhorn@gmail.com>
2019-12-06 11:41:58 -0500

    Update vectest.gd

--- vectest.gd
+++ vectest.gd
@@ -9,21 +9,21 @@ func _ready():
 	l.rotate(deg2rad(180))  # Face up
 	l.scale = Vector2(-1,1) # Flip along X axis
 	
-	var p0 = Vector2(0,0)
-	var p1 = p0 + STEP
+	var d = 0
+	var pos = Vector2(0,0)
+	
+	l.add_point(pos)
+	var p1 = pos + STEP
 	var p2 = p1 + STEP
 	var p3 = STEP.rotated(-DELTA + -DELTA) + p2
 	var p4 = STEP.rotated(-DELTA + -DELTA + DELTA) + p3
 	var p5 = STEP.rotated(-DELTA + -DELTA + DELTA + DELTA) + p4
 	
-	l.add_point(p0)
 	l.add_point(p1)
 	l.add_point(p2)
 	l.add_point(p3)
 	l.add_point(p4)
 	l.add_point(p5)
-
-
 
 	print(p1)
 	print(p2)
