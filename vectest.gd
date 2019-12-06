57998c60408f6abdc7cfb25ac0200ee86704c838
Terkwood <metaterkhorn@gmail.com>
2019-12-06 11:15:35 -0500

    Update vectest.gd

--- vectest.gd
+++ vectest.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 const STEP = Vector2(100,0)
+const DELTA = deg2rad(22.5)
 
 func _ready():
 	var l = Line2D.new()
 	l.width = 20
 	var p0 = Vector2(0,0)
-	var p1 = STEP.rotated(deg2rad(22.5))
-	var p2 = STEP.rotated(deg2rad(22.5 + 22.5)) + p1
-	var p3 = STEP.rotated(deg2rad(22.5 + 22.5 + 22.5)) + p2
+	var p1 = STEP.rotated(DELTA)
+	var p2 = STEP.rotated(DELTA + DELTA) + p1
+	var p3 = STEP.rotated(DELTA + DELTA + DELTA) + p2
 	
 	l.add_point(p0)
 	l.add_point(p1)
