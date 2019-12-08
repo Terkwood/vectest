9cea40e4fa5b7c73bae173cfac7348f677d65afb
Terkwood <metaterkhorn@gmail.com>
2019-12-08 14:54:22 -0500

    Update AutoLineMesh.gd
    Use an index

--- AutoLineMesh.gd
+++ AutoLineMesh.gd
@@ -1,20 +1,28 @@
 extends MeshInstance2D
 
-const WIDTH = 100
+const WIDTH = 30
 const STEP = 200
 const Z = Vector3(0,0,1)
 
 func draw_step(point, rot, st):
 	# Neg corner
 	st.add_vertex(Vector3(point.x, point.y, 0).rotated(Z,rot))
-	st.add_vertex(Vector3(point.x, point.y + STEP, 0).rotated(Z,rot))
-	st.add_vertex(Vector3(point.x + WIDTH, point.y + STEP, 0).rotated(Z,rot))
+	var final = Vector3(point.x, point.y + STEP, 0).rotated(Z,rot)
+	st.add_vertex(final)
+	
+	#st.add_vertex(Vector3(point.x + WIDTH, point.y + STEP, 0).rotated(Z,rot))
 
 	# Pos corner
 	st.add_vertex(Vector3(point.x + WIDTH, point.y + STEP, 0).rotated(Z,rot))
 	st.add_vertex(Vector3(point.x + WIDTH, point.y, 0).rotated(Z,rot))
-	var final = Vector3(point.x, point.y + STEP, 0).rotated(Z,rot)
-	st.add_vertex(final)
+	
+	st.add_index(0);
+	st.add_index(1);
+	st.add_index(2);
+	
+	st.add_index(0);
+	st.add_index(2);
+	st.add_index(3);
 	
 	return Vector2(final.x, final.y)
 	
@@ -34,7 +42,7 @@ func _ready():
 	var r = deg2rad(40)
 
 	var p1 = draw_step(Vector2(-111, -222), r, st)
-	#draw_step(p1, 2 * r, st)
+	draw_step(p1, 0, st)
 	
 	
 	mesh = st.commit()
