88fda3b77ee71cde4ab0617cd3e22d40e9d12c16
Terkwood <metaterkhorn@gmail.com>
2019-12-08 15:26:31 -0500

    Update AutoLineMesh.gd

--- AutoLineMesh.gd
+++ AutoLineMesh.gd
@@ -1,6 +1,6 @@
 extends MeshInstance2D
 
-const WIDTH = 30
+const WIDTH = 5
 const STEP = 200
 const Z = Vector3(0,0,1)
 
@@ -25,7 +25,7 @@ func draw_step(point, rot, st, i):
 	st.add_index(j + 3)
 	
 	return Vector2(final.x, final.y)
-	
+
 
 func _ready():
 	var st = SurfaceTool.new()
@@ -39,11 +39,10 @@ func _ready():
 	# Let's draw a rectangle from -200, -200 to 200, 200
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
 	
-	var r = deg2rad(-40)
+	var r = deg2rad(40)
 
-	var p1 = draw_step(Vector2(-111, -222), r, st, 0)
-	var p2 = draw_step(p1, deg2rad(-80), st, 1)
-	var p3 = draw_step(p2, deg2rad(-120), st, 2)
+	var p1 = draw_step(Vector2(0, -222), r, st, 0)
+	var p2 = draw_step(p1, deg2rad(80), st, 1)
+	var p3 = draw_step(p2, deg2rad(120), st, 2)
 
 	mesh = st.commit()
-	
